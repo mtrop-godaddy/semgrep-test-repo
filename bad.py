cd247626041297600d9f2832c7b738ca4c7f3866
mtrop-godaddy <mtrop@godaddy.com>
2024-10-08 12:32:06 +0100

    Bad Python crypto code

--- bad.py
+++ bad.py
@@ -1,4 +1,6 @@
 import os
+from cryptography.hazmat.primitives.asymmetric import dsa
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
 
 secret = "^IUhegeh957AEgf$^Aujeghnwe0283t2hyg0wdfhbsudb2409UHY4T9479(&y$t0H0h8"
 
@@ -15,3 +17,16 @@ subprocess.call('echo $HOME', shell=True)
 
 # throws an error
 subprocess.call('echo {} $HOME'.format(secret), shell=False)
+
+# ruleid: cryptography-dsa
+private_key = dsa.generate_private_key(
+    key_size=1024,
+)
+
+params = dsa.generate_parameters(2048)
+
+# ruleid: cryptography-dsa
+private_key = params.generate_private_key()
+
+# ok: cryptography-dsa
+private_key = Ed25519PrivateKey.generate()
