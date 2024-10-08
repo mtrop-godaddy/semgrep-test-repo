6302acd4a94cbc3d8a108c14cd6bf2ec19485c53
mtrop-godaddy <mtrop@godaddy.com>
2024-10-08 12:20:43 +0100

    Switch back to free version

--- bad.py
+++ bad.py
@@ -1,9 +1,15 @@
 import os
 
 # Vulnerable
-user_input = "foo && cat /etc/passwd" # value supplied by user
+user_input = input('What do you want to grep for?\n')
 os.system("grep -R {} .".format(user_input))
 
 # Vulnerable
-user_input = "foo && cat /etc/passwd" # value supplied by user
+user_input = input('Which directory do you want to list files in?\n')
 os.popen("ls -l " + user_input)
+
+# prints home directory
+subprocess.call('echo $HOME', shell=True)
+
+# throws an error
+subprocess.call('echo $HOME', shell=False)
